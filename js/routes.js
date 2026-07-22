00b745a89e3cced8caf28fbe428bea6569ec7940
zis76 <76632633+zis76@users.noreply.github.com>
2026-07-21 19:11:22 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
-import Roulette from './pages/cpleaderboard.js';
+import cpleaderboard from './pages/cpleaderboard.js';
 
 export default [
     { path: '/', component: List },
